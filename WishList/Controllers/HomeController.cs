3378edd19e4c199a6b6f8b15f5e47c0108a0b4b5
Matthew Guptil <matthew.d.guptil@exxonmobil.com>
2021-10-05 15:18:53 -0500

    sdvvdsvsd

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WishList
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
         }
     }
 }
